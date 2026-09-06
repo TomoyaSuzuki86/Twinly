e14937f2c15210ca4528b640f0127cb4f37c6dfa
TomoyaSuzuki86 <tmgo31586w@gmail.com>
2026-09-06 21:41:58 +0900

    Replace daily summary email with push notification (#58)
    * Replace daily summary email delivery with web push
    
    * Override daily summary email handlers with push delivery
    
    * Replace daily email settings UI with summary notifications
    
    * Update Premium copy for daily summary notifications
    
    * Update pricing tests for summary notifications
    
    * Disable legacy Resend delivery entrypoint

--- functions/main.js
+++ functions/main.js
@@ -1,5 +1,2 @@
 Object.assign(exports, require('./index'));
-
-if (process.env.TWINLY_EMAIL_DELIVERY_ENABLED === 'true') {
-  Object.assign(exports, require('./email-delivery'));
-}
+Object.assign(exports, require('./daily-summary-push'));
