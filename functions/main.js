1a8f3f9cba2d4b0ea9b7077d7ab5d2eb9ac7655f
TomoyaSuzuki86 <tmgo31586w@gmail.com>
2026-09-06 21:37:51 +0900

    Override daily summary email handlers with push delivery

--- functions/main.js
+++ functions/main.js
@@ -1,4 +1,5 @@
 Object.assign(exports, require('./index'));
+Object.assign(exports, require('./daily-summary-push'));
 
 if (process.env.TWINLY_EMAIL_DELIVERY_ENABLED === 'true') {
   Object.assign(exports, require('./email-delivery'));
