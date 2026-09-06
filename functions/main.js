451ee7ff49437d3bec286f0867f3de893e2d6a5a
TomoyaSuzuki86 <tmgo31586w@gmail.com>
2026-09-06 21:41:14 +0900

    Disable legacy Resend delivery entrypoint

--- functions/main.js
+++ functions/main.js
@@ -1,6 +1,2 @@
 Object.assign(exports, require('./index'));
 Object.assign(exports, require('./daily-summary-push'));
-
-if (process.env.TWINLY_EMAIL_DELIVERY_ENABLED === 'true') {
-  Object.assign(exports, require('./email-delivery'));
-}
