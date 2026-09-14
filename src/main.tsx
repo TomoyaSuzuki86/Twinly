7e85b417b6474e9dbfcfbb7e767737094ea75b43
TomoyaSuzuki86 <tmgo31586w@gmail.com>
2026-09-15 05:29:51 +0900

    refactor: remove global primary action enhancer mount

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AiAdviceLauncher } from "./components/AiAdviceLauncher";
-import { PrimaryActionMorphEnhancer } from "./components/PrimaryActionMorphEnhancer";
 import { installGlobalInputGuards } from "./lib/global-input-guards";
 import { installServiceWorkerRegistration } from "./lib/service-worker-registration";
 import "./index.css";
@@ -16,7 +15,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
     <AiAdviceLauncher />
-    <PrimaryActionMorphEnhancer />
   </React.StrictMode>
 );
 
